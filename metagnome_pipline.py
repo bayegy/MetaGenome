8c5edf39c343f01e708a6825925b10cc5d14ee81
bayegy <947366452@qq.com>
2019-04-02 15:08:26 +0800

    xiangku

--- metagnome_pipline.py
+++ metagnome_pipline.py
@@ -37,22 +37,23 @@ class MetagenomePipline(object):
     def _init_outdir_(self):
         os.system('perl ' + self.path['cii_home'] + 'create_dir_structure.pl ' + self.out_dir)
         for fq_path, new_id, direction in self.fq_info:
-            os.system(("ln {} " + self.out_dir + "Raw_fastq/{}_{}.fq.gz").format(fq_path, new_id, direction))
+            os.system(("ln -s {} " + self.out_dir + "Raw_fastq/{}_{}.fq.gz").format(fq_path, new_id, direction))
 
-    def map_list(self, pattern, each, only_r1=False):
+    def map_list(self, pattern=False, each=False, only_r1=False):
         out = []
         for fq_path, new_id, direction in self.fq_info:
+            ele = pattern.format(new_id, direction) if pattern else [new_id, direction]
             if direction == "R2":
                 if not only_r1:
-                    out.append(pattern.format(new_id, direction))
+                    out.append(ele)
             else:
-                out.append(pattern.format(new_id, direction))
+                out.append(ele)
         out.sort()
-        return split_list(out, each)
+        return split_list(out, each) if each else out
 
     def synchronize(func):
 
-        def wfunc(*agrs, fq_list, **kwargs):
+        def wfunc(*args, fq_list, first_check=10, **kwargs):
             print("######################Running " + str(func))
             global running_list
             # running_list = MetagenomePipline.out_dir + '.running_list'
@@ -61,8 +62,26 @@ class MetagenomePipline(object):
                 with open(running_list, 'w') as f:
                     f.write('\n'.join(fq))
                     f.flush()
-                func(*agrs, fq_list=running_list, **kwargs)
-                time.sleep(10 * 60)
+                func(*args, fq_list=running_list, **kwargs)
+                time.sleep(first_check * 60)
+                while True:
+                    if not list(os.popen("qstat")):
+                        break
+                    time.sleep(60)
+            end_time = time.time()
+            time_used = (end_time - start_time) / 60
+            print("######################" + str(func) + " done; time used: {} min".format(time_used))
+        return wfunc
+
+    def synchronize2(func):
+
+        def wfunc(*args, fq_list, first_check=10, **kwargs):
+            print("######################Running " + str(func))
+            # running_list = MetagenomePipline.out_dir + '.running_list'
+            start_time = time.time()
+            for fq in fq_list:
+                func(*args, fq_list=fq, **kwargs)
+                time.sleep(first_check * 60)
                 while True:
                     if not list(os.popen("qstat")):
                         break
@@ -114,13 +133,50 @@ class MetagenomePipline(object):
     def run_humann(self, fq_list: list):
         os.system(self.homized_cmd('perl run_humann.pl {} SE'.format(fq_list)))
 
-    def main(self, processor=2):
-        self.run_fastqc(self.raw_list, processor=processor)
-        self.run_trim(self.raw_list)
-        self.run_filter(self.trimmed_list)
-        self.run_fastq(self.filtered_list, processor=processor)
-        self.run_de_host(self.filtered_list, processor=processor)
-        self.run_merge_se_to_pe(self.de_host_r1_list)
-        self.run_kraken2(self.merged_pe_r1_list, processor=processor)
-        self.run_metaphlan2(self.merged_pe_r1_list, processor=processor)
-        self.run_humann(self.merged_pe_r1_list)
+    @synchronize2
+    def run_fmap(self, fq_list, database="protein_fasta_protein_homolog_model", processor=4, out_dir='FMAP'):
+        """
+        arguments:
+            database: database at FMAP_data
+        """
+        with open(self.path['fmap_home'] + '/FMAP_data/database', 'w') as f:
+            f.write(database)
+        out = self.out_dir + out_dir + '/'
+        for fq in fq_list:
+            cmd = "echo 'perl {}/FMAP_mapping.pl -p {} {} > {}.mapping.txt' | qsub -V -N {} -cwd -l h_vmem=3G -o {} -e {} -pe smp 4".format(
+                self.path['fmap_home'], str(processor), fq, out + os.path.basename(fq), fq, out, out)
+            print("submit:\n {}\n\n".format(cmd))
+            os.system(cmd)
+
+    def quantify_fmap(self, out_dir='FMAP', all_name="all.txt"):
+        out = self.out_dir + out_dir + '/'
+        file_pattern = out + "{}_R1.hg38.unmapped.{}.fastq.gz"
+        info_list = self.map_list(only_r1=True)
+        for new_id, direction in info_list:
+            os.system("perl {}/FMAP_quantification.pl {}.mapping.txt > {}.abundance.txt".format(
+                self.path['fmap_home'], file_pattern.format(new_id, direction), out_dir + new_id))
+        args = ["{}={}.abundance.txt".format(new_id, out_dir + new_id) for new_id, direction in info_list]
+        os.system("perl {}/FMAP_table.pl {} > {}".format(
+            self.path['fmap_home'], " ".join(args), out + all_name))
+
+    def fmap_wrapper(self, run_type="KEGG", processor=4):
+        if run_type == "KEGG":
+            self.run_fmap(fq_list=self.merged_pe_r1_list,
+                          database="orthology_uniref90_2_2157_4751.20180725040837", processor=processor, out_dir="FMAP")
+            self.quantify_fmap(out_dir="FMAP", all_name="All.Function.abundance.KeepID.KO.txt")
+        elif run_type == "AMR":
+            self.run_fmap(fq_list=self.merged_pe_r1_list, database="protein_fasta_protein_homolog_model",
+                          processor=processor, out_dir="AMR")
+            self.quantify_fmap(out_dir="AMR", all_name="All.AMR.abundance.txt")
+
+    def main(self, processor=4):
+        self.run_fastqc(fq_list=self.raw_list, processor=processor)
+        self.run_trim(fq_list=self.raw_list)
+        self.run_filter(fq_list=self.trimmed_list)
+        self.run_fastq(fq_list=self.filtered_list, processor=processor)
+        self.run_de_host(fq_list=self.filtered_list, processor=processor)
+        self.run_merge_se_to_pe(fq_list=self.de_host_r1_list)
+        self.run_kraken2(fq_list=self.merged_pe_r1_list, processor=processor)
+        self.run_metaphlan2(fq_list=self.merged_pe_r1_list, processor=processor)
+        self.run_humann(fq_list=self.merged_pe_r1_list)
+        self.run_fmap(fq_list=self.merged_pe_r1_list, processor=processor)
